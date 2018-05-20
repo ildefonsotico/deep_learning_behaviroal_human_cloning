fc6af1013be9bc30d56b94f48f033f5997ff8e04
Ildefonso Neto <ildefonsotico@hotmail.com>
2018-05-20 18:12:06 -0300

    It will be added 3 new files, each one represent a different net architecture

---
+++ model_simple_net_preprocess_data.py
@@ -0,0 +1,38 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for line in reader:
+        lines.append(line)
+
+images = []
+measurements = []
+for line in lines:
+    source_path=line[0]
+    filename = source_path.split('/')[-1]
+    current_path = '../data/IMG' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+
+X_train = np.array(images)
+Y_train = np.array(measurements)
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense
+from keras.layers import Lambda
+
+model = Sequential()
+model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, Y_train, validation_split=0.3, shuffle=True, nb_epoch=7)
+
+model.save('model_simple_net_w_preprocess_data.h5')
