4871a1fc140c32010bea3e04307b4202d7c192e5
Ildefonso Neto <ildefonsotico@hotmail.com>
2018-05-20 19:19:04 -0300

    MaxPooling was called wrongly

--- model_lenet.py
+++ model_lenet.py
@@ -31,14 +31,14 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 from keras.layers import Lambda
 from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPolling2D
+from keras.layers.pooling import MaxPooling2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 model.add(Convolution2D(6,5,5,activation='relu'))
-model.add(MaxPolling2D())
+model.add(MaxPooling2D())
 model.add(Convolution2D(6,5,5,activation='relu'))
-model.add(MaxPolling2D())
+model.add(MaxPooling2D())
 model.add(Flatten())
 model.add(Dense(120))
 model.add(Dense(84))
