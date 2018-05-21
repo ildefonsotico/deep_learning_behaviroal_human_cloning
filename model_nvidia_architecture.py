9d6eaec1a2243c4faed455d5698f9655c0811341
Ildefonso Neto <ildefonsotico@hotmail.com>
2018-05-20 21:54:10 -0300

    Bug fixes

--- model_nvidia_architecture.py
+++ model_nvidia_architecture.py
@@ -84,6 +84,7 @@ model.add(Flatten())
 model.add(Dense(100))
 model.add(Dense(50))
 model.add(Dense(10))
+model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, Y_train, validation_split=0.3, shuffle=True, nb_epoch=5)
