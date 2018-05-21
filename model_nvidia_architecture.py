3ae1a9a482ad123badf5140e4d9afa50a7858f88
Ildefonso Neto <ildefonsotico@hotmail.com>
2018-05-20 21:51:47 -0300

    Aditition of a new architecture

--- model_nvidia_architecture.py
+++ model_nvidia_architecture.py
@@ -24,9 +24,6 @@ for line in lines:
     img_center = cv2.imread(current_path + filename_center)
     img_left = cv2.imread(current_path + filename_left)
     img_right = cv2.imread(current_path + filename_right)
-    #print("Image Center: ", img_center)
-    #print("Image Left: ", img_left)
-    #print("Image Right: ", img_right)
 
     if img_center is None:
         print("Image Center path incorrect: ", img_center)
@@ -76,16 +73,17 @@ from keras.layers.pooling import MaxPooling2D
 from keras.layers import Cropping2D
 
 model = Sequential()
-model.add(Cropping2D(cropping=((50,20), (0,0)), input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5))
-model.add(Convolution2D(6,5,5,activation='relu'))
-model.add(MaxPooling2D())
-model.add(Convolution2D(6,5,5,activation='relu'))
-model.add(MaxPooling2D())
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
+model.add(Convolution2D(64,3,3,activation='relu'))
 model.add(Flatten())
-model.add(Dense(120))
-model.add(Dense(84))
-model.add(Dense(1))
+model.add(Dense(100))
+model.add(Dense(50))
+model.add(Dense(10))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, Y_train, validation_split=0.3, shuffle=True, nb_epoch=5)
